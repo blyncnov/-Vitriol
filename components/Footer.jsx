41d9396117f2912dd777ff118b3878a245c65a66
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-06 21:38:56 +0100

    anti vitriol charity webapplication v17 footer color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,46 +9,52 @@ import {
   FlexBoxToRow
 } from "../styles/constants/Constants"
 
+import { FooterContainer } from "../styles/styled/Footer"
+
 const Footer = () => {
   return (
-    <div>
-      <Constraints>
-        <Section>
-          <SectionInner align="flex-start">
-            <Divider>
-              <h1><span>!Vitriol</span></h1>
-              <br />
-              <Paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Modi cumque tenetur inventore veniam, hic vel ipsa
-                necessitatibus ducimus architecto fugiat!
-              </Paragraph>
-              <br />
-            </Divider>
+    <>
+      <FooterContainer>
+        <Constraints>
+          <Section>
+            <SectionInner align="flex-start">
+              <Divider>
+                <h1><span>!Vitriol</span></h1>
+                <br />
+                <Paragraph color="#8892B0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Modi cumque tenetur inventore veniam, hic vel ipsa
+                  necessitatibus ducimus architecto fugiat!
+                </Paragraph>
+                <br />
+              </Divider>
+              <SectionInner align="flex-start">
+                <Divider>
+                  <h3><span>Quick Links</span></h3>
+                  <br />
+                  {NavigationLists.map((item) => {
+                    return <Paragraph color="#8892B0" key={item.id}><a style={{ color: "inherit" }} href={item.navigate}>{item.name}</a></Paragraph>
+                  })}
+                </Divider>
 
-            <Divider>
-              <h3><span>Quick Links</span></h3>
-              <br />
-              {NavigationLists.map((item) => {
-                return <Paragraph key={item.id}><a style={{ color: "inherit" }} href={item.navigate}>{item.name}</a></Paragraph>
-              })}
-            </Divider>
+                <Divider >
+                  <h3><span>Contact Us</span></h3>
+                  <br />
+                  <Paragraph color="#8892B0">183 Agbede St. Funaab gate, Funaab, Alabata</Paragraph>
+                  <br />
+                  <Paragraph color="#8892B0">bholuwatife00@gmail.com</Paragraph>
+                  <br />
+                  <li>+234 (814) 9055 068</li>
+                  <br />
+                  <Paragraph color="#8892B0">	Monday — Friday 8:00am - 5:00pm</Paragraph>
+                </Divider>
+              </SectionInner>
 
-            <Divider >
-              <h3><span>Contact Us</span></h3>
-              <br />
-              <Paragraph>183 Agbede St. Funaab gate, Funaab, Alabata</Paragraph>
-              <br />
-              <Paragraph>bholuwatife00@gmail.com</Paragraph>
-              <br />
-              <li>+234 (814) 9055 068</li>
-              <br />
-              <Paragraph>	Monday — Friday 8:00am - 5:00pm</Paragraph>
-            </Divider>
-          </SectionInner>
-        </Section>
-      </Constraints>
-    </div>
+            </SectionInner>
+          </Section>
+        </Constraints>
+      </FooterContainer>
+    </>
   )
 }
 
---
+++ styles/styled/Footer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const FooterContainer  = styled.div`
+width: auto;
+background-color: #0A192F;
+position: relative;
+box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+color: #8892B0 !important;
+`
+
+export default FooterContainer
